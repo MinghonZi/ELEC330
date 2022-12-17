ebadf982a9ea2b259cac2d3a7a3545a63c754754
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2022-12-17 00:34:35 +0000

    Test performance of model
    The trained yolov5s model cannot detect the face of an anime
    character. The confidence is low when detecting real humans' face.

---
+++ inference.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import torch
+from rospkg import RosPack
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', Path(RosPack().get_path('ass2'))/'weights'/'yolov5s-face.pt')
+
+# img = 'https://ultralytics.com/images/zidane.jpg'
+img = './nekopara-pajama-chocola-and-vanilla.jpg'
+
+results = model(img)
+
+results.save()
